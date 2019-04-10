b3e3d3daf21a44fe21418eef4c99a7496965eef5
KrumecaHG <krumecahg@abv.bg>
2019-04-10 20:37:51 +0300

    Update registration.php

--- Rakeshmagento/Logcleaner/registration.php
+++ Rakeshmagento/Logcleaner/registration.php
@@ -2,6 +2,6 @@
 	
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	"Rakeshmagento_Logcleaner",
+	"Rakeshmagento_LogCleaner",
 	__DIR__
 );
